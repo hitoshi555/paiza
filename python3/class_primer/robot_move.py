519cb507a650ba96dce867a7314cae2474a00223
hitoshi555 <hitoshi1845@gmail.com>
2024-06-03 23:45:02 +0900

    wip

--- python3/class_primer/robot_move.py
+++ python3/class_primer/robot_move.py
@@ -1,3 +1,9 @@
 class Robot:
+    def __init__(self, level, x, y):
+        self.level = level
+        self.x = x
+        self.y = y
 
 h, w, n, k = [int(x) for x in input().split()]
+
+maps = [[0]*w]*h
